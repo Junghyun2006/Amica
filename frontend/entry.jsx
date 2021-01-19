a3fbc809ac47541326f759b1d86a2afd1fb05f31
junghyun2006 <jung.hyun.park516@gmail.com>
2021-01-19 17:53:33 -0500

    Test3

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM, { render } from 'react-dom';
-import configureStore from './store/store';
-import Root from './components/root';
+// import React from 'react';
+// import ReactDOM, { render } from 'react-dom';
+// import configureStore from './store/store';
+// import Root from './components/root';
 
-document.addEventListender("DOMContentLoaded", () => {
-    const root = document.getElementById('root');
-    const store = configureStore();
-    ReactDOM.render(<Root store={store} />, root)
-})
+// document.addEventListender("DOMContentLoaded", () => {
+//     const root = document.getElementById('root');
+//     const store = configureStore();
+//     ReactDOM.render(<Root store={store} />, root)
+// })
