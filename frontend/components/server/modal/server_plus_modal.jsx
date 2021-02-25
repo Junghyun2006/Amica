1c5eb662981ce1d57a11a40f43cd5b6c7fa0a912
junghyun2006 <jung.hyun.park516@gmail.com>
2021-02-25 14:42:11 -0500

    finish server plus modal

--- frontend/components/server/modal/server_plus_modal.jsx
+++ frontend/components/server/modal/server_plus_modal.jsx
@@ -2,7 +2,38 @@ import React from "react";
 
 const ServerPlusModal = (props) => {
     return (
-        <div className="server-plus-modal">hi</div>
+        <div className="server-plus-modal">
+            <h1 className="server-plus-title">Create a server</h1>
+            <h2 className="server-plus-description">Your server is where you and your friends hang out. Make yours and start talking.</h2>
+            <div className="server-plus-option-container">
+                <div className="server-plus-option">
+                    <img className="create-server-img" src={window.createServerSvg}/>
+                    <h1>Create My Own</h1>
+                    <img className="server-plus-arrow" src={window.serverPlusArrow}/>
+                </div>
+                <div className="start-template">START FROM A TEMPLATE</div>
+                <div className="server-plus-option">
+                    <img className="create-server-img" src={window.spGaming}/>
+                    <h1>Gaming</h1>
+                    <img className="server-plus-arrow" src={window.serverPlusArrow}/>
+                </div>
+                <div className="server-plus-option">
+                    <img className="create-server-img" src={window.spSchoolclub}/>
+                    <h1>School Club</h1>
+                    <img className="server-plus-arrow" src={window.serverPlusArrow}/>
+                </div>
+                <div className="server-plus-option">
+                    <img className="create-server-img" src={window.spStudygroup}/>
+                    <h1>Study Group</h1>
+                    <img className="server-plus-arrow" src={window.serverPlusArrow}/>
+                </div>
+            </div>
+            <div className="sp-join-container">
+                <h1>Have an invite already?</h1>
+                <button>Join a Server</button>
+            </div>
+            
+        </div>
     )
 }
 
