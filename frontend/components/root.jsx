a7ab44243d2879d8cd9f6fb7276066d8645c174a
junghyun2006 <jung.hyun.park516@gmail.com>
2021-03-29 01:12:04 -0400

    modify root

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,21 +2,13 @@ import React, { createContext } from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter, BrowserRouter } from 'react-router-dom';
 import App from './app';
-import actionCable from 'actioncable';
-
-const CableApp = {};
-CableApp.cable = actionCable.createConsumer('ws://localhost:3000/cable') // change to port im using
-export const ActionCableContext = createContext()
-
 
 
 const Root = ({store}) => (
     <Provider store={store}>
-        <ActionCableContext.Provider value={CableApp.cable}>
-            <HashRouter>
-                <App />
-            </HashRouter>
-        </ActionCableContext.Provider>
+        <HashRouter>
+            <App />
+        </HashRouter>
     </Provider>
 );
 
