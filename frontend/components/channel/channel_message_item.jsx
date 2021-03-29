080f18c58ba5494bbdc4d82268855f4b46bd1aa0
junghyun2006 <jung.hyun.park516@gmail.com>
2021-03-28 21:37:31 -0400

    added message avatars

--- frontend/components/channel/channel_message_item.jsx
+++ frontend/components/channel/channel_message_item.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const ChannelMessageItem = ({message}) => {
     return (
         <>  
+            <img src={message.avatarUrl} className="avatar-icon" />
             <h1>{message.username}</h1>
             <h1>{message.message_body}</h1>
         </>
